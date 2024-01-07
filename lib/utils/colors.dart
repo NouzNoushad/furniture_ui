d95e883ecc68267b48a0463437cf21fad79d2e75
NouzNoushad <77887277+NouzNoushad@users.noreply.github.com>
2024-01-07 20:17:11 +0530

    furniture ui design

---
+++ lib/utils/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class ColorPicker {
+  static const Color backgroundColor = Color.fromRGBO(92, 97, 130, 1);
+  static const Color primaryColor = Colors.white;
+  static const Color secondaryColor = Color.fromRGBO(250, 203, 154, 1);
+}
---
+++ lib/utils/constants.dart
@@ -0,0 +1,17 @@
+List<Map<String, dynamic>> chairs = [
+  {
+    'title': 'Simple Chair',
+    'image': 'chair2.png',
+    'price': 75,
+  },
+  {
+    'title': 'Sofa Chair',
+    'image': 'chair4.png',
+    'price': 1200,
+  },
+  {
+    'title': 'Modern Chair',
+    'image': 'chair1.png',
+    'price': 2500,
+  },
+];
